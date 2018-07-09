418f7b219ad4ec9ea31e44467a2f3faca02525b9
Lwazi Tongo <jambilwah@gmail.com>
2018-07-09 14:40:21 +0200

    intiail commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WinnerPage } from '../winner/winner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  scoreForteamA=0;
+  scoreForteamB=0;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+ teamA(value:number):void{
+   this.scoreForteamA += value;
+
+ }
+ teamB(value:number):void{
+  this.scoreForteamB += value;
+
+}
+
+reset():void{
+
+ this.scoreForteamA=0;
+ this.scoreForteamB=0;
+ }
+ end():void{
+
+  this.navCtrl.push(WinnerPage);
+
+ }
 }
